f91e60f432fb4bc59c28cf14aa7234236f435354
Ayako-q <ischenkalex@gmail.com>
2022-10-12 21:35:32 +0300

    Initial commit

---
+++ src/main/java/com/example/project9/Main.java
@@ -0,0 +1,11 @@
+package com.example.project9;
+//Part 1
+//1.1 Creating a new class and its object
+public class bicycle {
+    //objects
+    public String brand;
+    public String model;
+    public int modelYear;
+
+}
+
